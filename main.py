5b1dde59a410a028d71b14886b361b76261eb8f0
Vitaly Kalinin <vitaly.kalinin@icloud.com>
2024-07-31 15:13:03 +0100

    Add open files buttons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
 
 from ui_main import Ui_DiffChecker
 
@@ -12,6 +12,28 @@ class FileDiffChecker(QWidget):
         self.ui = Ui_DiffChecker()
         self.ui.setupUi(self)
 
+        self.file1_content = ""
+        self.file2_content = ""
+
+        # Buttons
+        self.ui.openFile1_btn.clicked.connect(self.open_file1)
+        self.ui.openFile2_btn.clicked.connect(self.open_file2)
+
+
+    def open_file1(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open File 1")
+        if file_name:
+            with open(file_name, 'r') as file:
+                self.file1_content = file.read()
+                self.ui.file1_form.setPlainText(self.file1_content)
+
+
+    def open_file2(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open File 2")
+        if file_name:
+            with open(file_name, 'r') as file:
+                self.file2_content = file.read()
+                self.ui.file2_form.setPlainText(self.file2_content)
 
 
 if __name__ == "__main__":
